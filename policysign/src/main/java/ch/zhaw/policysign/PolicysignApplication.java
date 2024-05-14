3182cefdd98c2abbec39444e62c30b4a687d00da
MarcWeberFS <marc.robin.weber@gmail.com>
2024-05-14 11:35:52 +0200

    init java

---
+++ policysign/src/main/java/ch/zhaw/policysign/PolicysignApplication.java
@@ -0,0 +1,13 @@
+package ch.zhaw.policysign;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PolicysignApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PolicysignApplication.class, args);
+	}
+
+}
---
+++ policysign/src/test/java/ch/zhaw/policysign/PolicysignApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.zhaw.policysign;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PolicysignApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
