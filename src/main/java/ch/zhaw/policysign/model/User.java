203131a16445f443d33e543d50e102bb50c0c82b
MarcWeberFS <marc.robin.weber@gmail.com>
2024-05-18 16:20:49 +0200

    user model created

---
+++ src/main/java/ch/zhaw/policysign/model/User.java
@@ -0,0 +1,19 @@
+package main.java.ch.zhaw.policysign.model;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+import lombok.Getter;
+import lombok.Setter;
+import java.util.List;
+
+@Getter
+@Setter
+@Document
+public class User {
+
+    @Id
+    private String id;
+    private String username;
+    private String email;
+    private List<String> roles;
+}
