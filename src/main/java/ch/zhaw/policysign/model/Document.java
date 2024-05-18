c08ec94da7702c5525261ad3f177586582630d78
MarcWeberFS <marc.robin.weber@gmail.com>
2024-05-18 16:30:34 +0200

    added document model

---
+++ src/main/java/ch/zhaw/policysign/model/Document.java
@@ -0,0 +1,34 @@
+package main.java.ch.zhaw.policysign.model;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+
+import java.util.Date;
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Document(collection = "policysign")
+public class Document {
+
+    @Id
+    private String id;
+    private String title;
+    private String description;
+    private String documentBase64;
+    private DocumentStatus status = DocumentStatus.PENDING;
+    private Date creationDate;
+    private Date updateDate;
+    private int xSignature;
+    private int ySignature;
+    private int signatureWidth;
+    private int signaturePage;
+    private String userId;
+    private String signedByEmail;
+}
---
+++ src/main/java/ch/zhaw/policysign/model/DocumentStatus.java
@@ -0,0 +1,5 @@
+package main.java.ch.zhaw.policysign.model;
+
+public enum DocumentStatus {
+    PENDING, SIGNED
+}
