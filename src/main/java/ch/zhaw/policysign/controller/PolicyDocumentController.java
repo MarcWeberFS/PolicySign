43a1e5514b4c08c6019c5614fd6fc6d5e503304f
MarcWeberFS <marc.robin.weber@gmail.com>
2024-05-21 14:05:13 +0200

    implemented the email sending

--- src/main/java/ch/zhaw/policysign/controller/PolicyDocumentController.java
+++ src/main/java/ch/zhaw/policysign/controller/PolicyDocumentController.java
@@ -1,12 +1,16 @@
 package ch.zhaw.policysign.controller;
 
+import ch.zhaw.policysign.model.DocumentStatus;
 import ch.zhaw.policysign.model.PolicyDocument;
 import ch.zhaw.policysign.service.PolicyDocumentService;
+import ch.zhaw.policysign.service.EmailService;
 
 import java.util.Date;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
+import jakarta.mail.MessagingException;
 
 @RestController
 @RequestMapping("/api/upload")
@@ -16,10 +20,30 @@ public class PolicyDocumentController {
     @Autowired
     private PolicyDocumentService policyDocumentService;
 
+    @Autowired
+    private EmailService emailService;
+
     @PostMapping
-    public PolicyDocument submitPolicyDocument(@RequestBody PolicyDocument policyDocument) {
+    public PolicyDocument submitPolicyDocument(@RequestBody PolicyDocument policyDocument) throws MessagingException {
         policyDocument.setCreationDate(new Date());
         policyDocument.setUpdateDate(new Date());
-        return policyDocumentService.savePolicyDocument(policyDocument);
+        policyDocument.setStatus(DocumentStatus.PENDING);
+        PolicyDocument savedDocument = policyDocumentService.savePolicyDocument(policyDocument);
+
+        // Prepare the HTML email content
+        String emailContent = "<html>" +
+                "<body>" +
+                "<h3>Dear User,</h3>" +
+                "<p>A new policy document requires your signature.</p>" +
+                "<p><b>Description:</b> " + savedDocument.getDescription() + "</p>" +
+                "<p>Please review and sign the document at your earliest convenience.</p>" +
+                "<p>Thank you.</p>" +
+                "</body>" +
+                "</html>";
+
+        // Send email notification
+        emailService.sendHtmlEmail(savedDocument.getSignedByEmail(), savedDocument.getTitle(), emailContent);
+
+        return savedDocument;
     }
 }
